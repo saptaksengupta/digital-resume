6894d7d3a156a838e0821fe5b873785731d21820
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-07-11 17:50:12 +0530

    [development] feature: responsiveness 🎯

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../sass/common.scss';
 import '../sass/_variables.scss';
+import '../sass/_mixins.scss';
 import '../sass/textAnimation.scss';
 import '../sass/pageContent.scss';
 import '../sass/slider.scss'
